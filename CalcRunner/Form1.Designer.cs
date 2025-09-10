9e2114be8f4373fdf6d0a10671eae656f74db2ba
Dimka <godslim1992@gmail.com>
2025-09-10 23:40:54 +0300

    Добавьте файлы проекта.

---
+++ CalcRunner/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CalcRunner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.myProcess = new System.Diagnostics.Process();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(300, 134);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(203, 80);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(300, 256);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(203, 80);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // myProcess
+            // 
+            this.myProcess.StartInfo.Domain = "";
+            this.myProcess.StartInfo.LoadUserProfile = false;
+            this.myProcess.StartInfo.Password = null;
+            this.myProcess.StartInfo.StandardErrorEncoding = null;
+            this.myProcess.StartInfo.StandardOutputEncoding = null;
+            this.myProcess.StartInfo.UserName = "";
+            this.myProcess.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Calc Runner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Diagnostics.Process myProcess;
+    }
+}
+
---
+++ CalcRunner/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CalcRunner
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            myProcess = new Process();
+            //myProcess.StartInfo = new ProcessStartInfo("calc.exe");
+            myProcess.StartInfo = new ProcessStartInfo("D:\\Programs\\7-Zip\\7zFM.exe");
+            myProcess.Start();
+        }
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            if (myProcess == null || myProcess.HasExited)
+            {
+                MessageBox.Show("Процесс уже завершён.");
+                return;
+            }
+
+            myProcess.CloseMainWindow();
+
+            myProcess.WaitForExit();
+            MessageBox.Show("Код завершения: " + myProcess.ExitCode.ToString());
+
+            myProcess.Close();
+            myProcess = null;
+        }
+    }
+}

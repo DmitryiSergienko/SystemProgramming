fc0ed28dd9f6c1324800501c278874eba1be71a5
Dimka <godslim1992@gmail.com>
2025-09-19 21:24:52 +0300

    Start ConcurrentBagParallelFor

---
+++ ConcurrentBagParallelFor/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+    pt4.pdf
+    Array18-32 по списку
+    Выполнить задание с обычным циклом (массивом) и СoncurrentBag с ParallelFor
+    Сравнить их по времени (размер массива регулируйте сами, но большой)
+    Измерение времени пока через класс StopWatch
+
+    Array32◦
+    Дан массив размера N. Найти номер его первого локального минимума (локальный минимум — это элемент, который меньше любого из
+    своих соседей).*/
+using System.Collections.Concurrent;
+
+Random rand = new Random();
+const int COUNT = 100;
+
+int[] array = new int[COUNT];
+ConcurrentBag<int> cb = new ConcurrentBag<int>();
+
+for (int i = 0; i < COUNT; i++)
+{
+    array[i] = rand.Next(1, 101);
+    cb.Add(array[i]);
+}
+
+Console.WriteLine(String.Join(" ", cb));
---
+++ ParallelExample/ParallelExample/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using static System.Console;
+
+namespace ParallelExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 2 million
+            var limit = 2_000_000;
+            // var limit = 200;
+            var numbers = Enumerable.Range(0, limit).ToList();
+
+            var watch = Stopwatch.StartNew();
+            var primeNumbersFromForeach = GetPrimeList(numbers);
+            watch.Stop();
+
+            var watchForParallel = Stopwatch.StartNew();
+            var primeNumbersFromParallelForeach = GetPrimeListWithParallel(numbers);
+            watchForParallel.Stop();
+
+            //WriteLine("Обычный List  и Where");
+            //WriteLine(String.Join(", ", primeNumbersFromForeach));
+            WriteLine($"Classical foreach loop | Total prime numbers : {primeNumbersFromForeach.Count} | Time Taken : {watch.ElapsedMilliseconds} ms.");
+            //WriteLine("\nConcurrentBag и ParallelFor");
+            //WriteLine(String.Join(", ", primeNumbersFromParallelForeach));
+            WriteLine($"Parallel.ForEach loop  | Total prime numbers : {primeNumbersFromParallelForeach.Count} | Time Taken : {watchForParallel.ElapsedMilliseconds} ms.");
+
+            WriteLine("Press 'Enter' to exit.");
+            ReadLine();
+        }
+
+        /// <summary>
+        /// GetPrimeList returns Prime numbers by using sequential ForEach
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <returns></returns>
+        private static IList<int> GetPrimeList(IList<int> numbers) => numbers.Where(IsPrime).ToList();
+
+        /// <summary>
+        /// GetPrimeListWithParallel returns Prime numbers by using Parallel.ForEach
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        private static IList<int> GetPrimeListWithParallel(IList<int> numbers)
+        {
+            var primeNumbers = new ConcurrentBag<int>();
+
+            Parallel.ForEach(numbers, number =>
+            {
+                if (IsPrime(number))
+                {
+                    primeNumbers.Add(number);
+                }
+            });
+
+            return primeNumbers.ToList();
+        }
+
+        /// <summary>
+        /// IsPrime returns true if number is Prime, else false.(https://en.wikipedia.org/wiki/Prime_number)
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        private static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            for (var divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}

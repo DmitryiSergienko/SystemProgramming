03c16d45fc41febe5cefbcefc52ad71efd718679
Dimka <godslim1992@gmail.com>
2025-09-22 17:08:40 +0300

    19.09.2025

--- ConcurrentBagParallelFor/Program.cs
+++ ConcurrentBagParallelFor/Program.cs
@@ -6,20 +6,79 @@
     Измерение времени пока через класс StopWatch
 
     Array32◦
-    Дан массив размера N. Найти номер его первого локального минимума (локальный минимум — это элемент, который меньше любого из
-    своих соседей).*/
+    Дан массив размера N. Найти номер его первого локального минимума 
+    (локальный минимум — это элемент, который меньше любого из своих соседей).
+*/
 using System.Collections.Concurrent;
+using System.Diagnostics;
 
-Random rand = new Random();
-const int COUNT = 100;
-
-int[] array = new int[COUNT];
-ConcurrentBag<int> cb = new ConcurrentBag<int>();
-
-for (int i = 0; i < COUNT; i++)
+internal class Program
 {
-    array[i] = rand.Next(1, 101);
-    cb.Add(array[i]);
-}
+    private static void Main(string[] args)
+    {
+        Random rand = new Random();
+        const int COUNT = 100_000_000;
 
-Console.WriteLine(String.Join(" ", cb));
+        int[] array = new int[COUNT];
+        ConcurrentBag<int> cb = new ConcurrentBag<int>();
+
+        var stopwatchArray = Stopwatch.StartNew();
+        Task arrayTask = Task.Run(() =>
+        {
+            for (int i = 0; i < COUNT; i++)
+            {
+                array[i] = rand.Next(1, 101);
+            }
+
+            int localMinArray = SearchFirstLocalMinimum(array);
+            if (localMinArray > 0)
+            {
+                Console.WriteLine($"Индекс первого локального минимума для массива: {localMinArray} - {array[localMinArray]}");
+            }
+            else
+            {
+                Console.WriteLine("Локальный минимум для массива отсутствует");
+            }
+        });
+        arrayTask.Wait();
+        stopwatchArray.Stop();
+        
+        var stopwatchCB = Stopwatch.StartNew();
+        Task cbTask = Task.Run(() =>
+        {
+            for (int i = 0; i < COUNT; i++)
+            {
+                cb.Add(array[i]);
+            }
+
+            int localMinCB = SearchFirstLocalMinimum(cb.ToArray());
+            if (localMinCB > 0)
+            {
+                Console.WriteLine($"Индекс первого локального минимума для списка: {localMinCB} - {cb.ToArray()[localMinCB]}");
+            }
+            else
+            {
+                Console.WriteLine("Локальный минимум для списка отсутствует");
+            }
+        });
+        cbTask.Wait();
+        stopwatchCB.Stop();
+
+        //Console.WriteLine("\nЭлементы массива:\n" + string.Join(" ", array));
+        //Console.WriteLine("\nЭлементы списка:\n" + string.Join(" ", cb));
+
+        Console.WriteLine($"\nВремя на выполнение алгоритма с массивом: {stopwatchArray.ElapsedMilliseconds} мс");
+        Console.WriteLine($"Время на выполнение алгоритма со списком: {stopwatchCB.ElapsedMilliseconds} мс");        
+    }
+    public static int SearchFirstLocalMinimum(int[] arr)
+    {
+        for (int i = 1; i < arr.Length - 1; i++)
+        {
+            if (arr[i] < arr[i - 1] && arr[i] < arr[i + 1])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
